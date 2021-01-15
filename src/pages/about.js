b735e9103f76443b949a9a394d80f779de15aaf3
shivenk78 <kumar.shiven15@gmail.com>
2021-01-15 18:15:21 -0500

    Added global CSS

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+    return (
+        <div style = {{color:'teal'}}>
+            <Header headerText="About this Site"/>
+            <p>Not much here, tbh, just following a tutorial.</p>
+            <Header headerText="This was a pretty fun excercise."/>
+        </div>
+    )
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Header from "../components/header"
+import {Link} from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{ color: `purple`}}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Welcome to home page!"/>
+      <p>What a world we live in.</p>
+      <img src="https://i.imgflip.com/2jjfsl.jpg"/>
+    </div>
 }
