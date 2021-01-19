369ed69ab25da4e56abca4d6eb4fc075c151c808
shivenk78 <kumar.shiven15@gmail.com>
2021-01-19 12:30:15 -0500

    Updated formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return (
-    <Layout>
-      <Header headerText="Welcome to the home page!"/>
-      <p>What a world we live in.</p>
-      <img src="https://i.imgflip.com/2jjfsl.jpg" alt="Gatsby meme"/>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Header headerText="Welcome to the home page!" />
+            <p>What a world we live in.</p>
+            <img src="https://i.imgflip.com/2jjfsl.jpg" alt="Gatsby meme" />
+        </Layout>
+    )
 }
